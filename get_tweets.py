8bcfdf4c746d2a878d62c183297a025d367f8f55
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2020-03-08 14:27:14 -0400

    Updated database

--- get_tweets.py
+++ get_tweets.py
@@ -22,7 +22,7 @@ try:
 
 except FileNotFoundError:
     # If file error occurs
-    print('Keys.txt not Found.')
+    print('keys.txt not Found.')
     sys.exit(1)
 
 # Connect to the API 
@@ -55,7 +55,7 @@ conn.commit()
 # For each person, we want to do initial offset + 250 hours
 time_format = '%Y-%m-%d %H:%M:%S'
 NUMBER_OF_BATCHES = 250
-HOUR_OFFSET = 466
+HOUR_OFFSET = 716
 
 # Determine start and end times
 start_time = datetime.datetime(2020, 2, 7, 0, 0, 0) + datetime.timedelta(hours=HOUR_OFFSET)
