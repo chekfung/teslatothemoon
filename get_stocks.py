7c3ee167deaeec31a9af90a48c80edb90ca7c084
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2020-03-08 16:30:52 -0400

    Update get_stocks.py

--- get_stocks.py
+++ get_stocks.py
@@ -8,7 +8,6 @@ import sqlite3
 # DataFrame index is the date, Columns include Open, High, Low, Close, Adj Close
 # and Volume
 data = yf.download("TSLA", start="2020-02-01", end="2020-02-29", interval="15m")
-print(data[:20])
 
 # Create connection to database
 conn = sqlite3.connect('stock_data.db')
@@ -30,7 +29,7 @@ data.to_sql('StockData', conn, if_exists='replace', index=True, \
     index_label="date", dtype=data_types)
 
 # Writes the DataFrame to a csv file for the data sample deliverable
-data.to_csv(path_or_buf='one_month_stock_data.csv')
+data[0:50].to_csv(path_or_buf='one_month_stock_data.csv')
 
 rnn_data = yf.download("TSLA", start="2019-10-01", end="2020-01-31", interval="1h")
 
@@ -42,4 +41,4 @@ c = conn.cursor()
 rnn_data.to_sql('RNNData', conn, if_exists='replace', index=True, index_label="date", dtype=data_types)
 
 # Writes the RNN training data DataFrame to a csv file for the sample deliverable
-rnn_data.to_csv(path_or_buf='rnn_stock_data.csv')
+rnn_data[0:50].to_csv(path_or_buf='rnn_stock_data.csv')
