41e65dd1e9ff37485ed0c0405e80cc2a09d27b78
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2020-03-08 16:00:27 -0400

    Updated Databases

--- get_stocks.py
+++ get_stocks.py
@@ -7,7 +7,7 @@ import sqlite3
 # Get Tesla stock data as a DataFrame from start date to end date
 # DataFrame index is the date, Columns include Open, High, Low, Close, Adj Close
 # and Volume
-data = yf.download("TSLA", start="2020-02-01", end="2020-02-29")
+data = yf.download("TSLA", start="2020-02-01", end="2020-02-29", interval="15m")
 data.index = Series.dt.strftime(data.index, '%Y-%m-%d')
 
 # Create connection to database
@@ -32,7 +32,7 @@ data.to_sql('StockData', conn, if_exists='replace', index=True, \
 # Writes the DataFrame to a csv file for the data sample deliverable
 data.to_csv(path_or_buf='one_month_stock_data.csv')
 
-rnn_data = yf.download("TSLA", start="2019-09-01", end="2020-01-31")
+rnn_data = yf.download("TSLA", start="2019-10-01", end="2020-01-31", interval="1h")
 rnn_data.index = Series.dt.strftime(rnn_data.index, '%Y-%m-%d')
 
 # Create connection to database
@@ -40,7 +40,7 @@ conn = sqlite3.connect('rnn_data.db')
 c = conn.cursor()
 
 # Writes the RNN training data DataFrame to a sqlite3 database
-data.to_sql('RNNData', conn, if_exists='replace', index=True, index_label="date", dtype=data_types)
+rnn_data.to_sql('RNNData', conn, if_exists='replace', index=True, index_label="date", dtype=data_types)
 
 # Writes the RNN training data DataFrame to a csv file for the sample deliverable
-data.to_csv(path_or_buf='rnn_stock_data.csv')
+rnn_data.to_csv(path_or_buf='rnn_stock_data.csv')
