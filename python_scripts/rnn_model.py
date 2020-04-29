dc68838ca071042d0a6efb1b88761391a554cf51
jchen20 <43321176+jchen20@users.noreply.github.com>
2020-04-29 11:54:09 -0400

    Update rnn_model.py

--- python_scripts/rnn_model.py
+++ python_scripts/rnn_model.py
@@ -15,6 +15,8 @@ class Stock_RNN(tf.keras.Model):
 		self.model.add(tf.keras.layers.Dropout(0.2))
 		self.model.add(tf.keras.layers.LSTM(64, return_sequences=False)) # LSTM layer
 		self.model.add(tf.keras.layers.Dropout(0.2))
+		self.model.add(tf.keras.layers.LSTM(32, return_sequences=False)) # LSTM layer
+		self.model.add(tf.keras.layers.Dropout(0.2))
 		self.model.add(tf.keras.layers.Dense(16, use_bias=True, kernel_initializer='uniform', activation='relu')) # Dense layer
 		self.model.add(tf.keras.layers.Dense(1, use_bias=True, kernel_initializer='uniform')) # Dense layer
 	@tf.function
