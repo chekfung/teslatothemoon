cbbbaa915ea0ada473bb7d0e4f54a92f1a64507d
Jason Ho <jason_ho@brown.edu>
2020-05-05 15:32:54 -0400

    RNN model includes twitter testing now

--- python_scripts/rnn_model.py
+++ python_scripts/rnn_model.py
@@ -3,6 +3,11 @@ import tensorflow as tf
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+from preprocess import Preprocess
 
 class Stock_RNN(tf.keras.Model):
     def __init__(self, batch_size):
@@ -12,26 +17,36 @@ class Stock_RNN(tf.keras.Model):
         self.batch_size = batch_size # Take one day's worth of data at a time
         self.model = tf.keras.Sequential()
         
-        self.model.add(tf.keras.layers.LSTM(128, return_sequences=True)) # LSTM layer
-        self.model.add(tf.keras.layers.Dropout(0.2))
-        self.model.add(tf.keras.layers.LSTM(64, return_sequences=True)) # LSTM layer
-        self.model.add(tf.keras.layers.Dropout(0.2))
-        self.model.add(tf.keras.layers.Flatten())
-        self.model.add(tf.keras.layers.Dense(100, use_bias=True, activation='relu')) # Dense layer
-        self.model.add(tf.keras.layers.Dense(100, use_bias=True, activation='relu'))
-        self.model.add(tf.keras.layers.Dropout(0.2))
-        self.model.add(tf.keras.layers.Dense(16, use_bias=True, kernel_initializer='uniform', activation='relu')) # Dense layer
-        self.model.add(tf.keras.layers.Dense(1, use_bias=True, kernel_initializer='uniform')) # Dense layer
-        
-        # self.model.add(tf.keras.layers.Conv2D(20, 2, padding='same'))
+        # self.model.add(tf.keras.layers.LSTM(128, return_sequences=True)) # LSTM layer
         # self.model.add(tf.keras.layers.Dropout(0.2))
-        # self.model.add(tf.keras.layers.Conv2D(40, 2, strides=(2,2), padding='same'))
+        # self.model.add(tf.keras.layers.LSTM(128, return_sequences=True)) # LSTM layer
         # self.model.add(tf.keras.layers.Dropout(0.2))
         # self.model.add(tf.keras.layers.Flatten())
         # self.model.add(tf.keras.layers.Dense(100, use_bias=True, activation='relu')) # Dense layer
-        # self.model.add(tf.keras.layers.Dense(100, use_bias=True, activation='relu'))
+        # self.model.add(tf.keras.layers.Dense(128, use_bias=True, activation='relu'))
         # self.model.add(tf.keras.layers.Dropout(0.2))
-        # self.model.add(tf.keras.layers.Dense(1, use_bias=True)) # Dense layer
+        # self.model.add(tf.keras.layers.Dense(16, use_bias=True, kernel_initializer='uniform', activation='relu')) # Dense layer
+        # self.model.add(tf.keras.layers.Dense(1, use_bias=True, kernel_initializer='uniform')) # Dense layer
+        
+        # Convolutional Layers
+        self.model.add(tf.keras.layers.Conv2D(16, 2, padding='same', activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.2))
+
+        self.model.add(tf.keras.layers.Conv2D(32, 2, strides=(2,2), padding='same', activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.2))
+
+        self.model.add(tf.keras.layers.Conv2D(64, 2, strides=(2,2), padding='same', activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.2))
+
+        self.model.add(tf.keras.layers.Conv2D(128, 2, strides=(2,2), padding='same', activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.2))
+
+        # Head
+        self.model.add(tf.keras.layers.Flatten())
+        self.model.add(tf.keras.layers.Dense(100, use_bias=True, activation='relu')) # Dense layer
+        self.model.add(tf.keras.layers.Dropout(0.2))
+        self.model.add(tf.keras.layers.Dense(100, use_bias=True, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(1, use_bias=True)) # Dense layer
         
     @tf.function
     def call(self, stock_input):
@@ -67,6 +82,10 @@ def train(model, train_data, train_prices, test_data, test_prices, num_epochs):
         test_predictions = model.call(test_data)
         print("Current Train MSE on epoch",current_epoch,":",model.accuracy_function(train_predictions, train_prices))
         print("Current Test MSE on epoch",current_epoch,":",model.accuracy_function(test_predictions, test_prices))
+
+        # TODO: Lowest so far I have found is 426
+        if model.accuracy_function(test_predictions, test_prices) < 410:
+            break
         current_epoch += 1
     pass
 
@@ -76,16 +95,56 @@ def test(model,test_data, test_prices):
     print("Test MSE:",mse)
     
 def get_data(test_prob=0.2):
-    conn = sqlite3.connect("../data/rnn_data.db")
-    data = pd.read_sql("SELECT * FROM RNNData", conn).to_numpy()
-    data_without_date = data[:,1:].astype(np.float32)
-    total_points = np.shape(data_without_date)[0]
-    train_data = data_without_date[:int((1-test_prob)*total_points)]
-    test_data = data_without_date[int((1-test_prob)*total_points):]
-    train_prices = train_data[1:,3]
-    test_prices = test_data[1:,3]
-    train_data = train_data[:-1]
-    test_data = test_data[:-1]
+    # FIXME: Just uncomment this stuff out
+    # conn = sqlite3.connect("../data/rnn_data.db")
+    # data = pd.read_sql("SELECT * FROM RNNData", conn).to_numpy()
+    # data_without_date = data[:,1:].astype(np.float32)
+    # total_points = np.shape(data_without_date)[0]
+    # train_data = data_without_date[:int((1-test_prob)*total_points)]
+    # test_data = data_without_date[int((1-test_prob)*total_points):]
+    # train_prices = train_data[1:,3]
+    # test_prices = test_data[1:,3]
+    # train_data = train_data[:-1]
+    # test_data = test_data[:-1]
+
+    # Preprocess the actual data
+    # Parsing actual data that we will use
+    STOCK_DATABASE_PATH = "../data/stock_data.db"
+    RNN_DATABASE_PATH = "../data/rnn_data.db"
+    x = Preprocess(STOCK_DATABASE_PATH, RNN_DATABASE_PATH)
+    numpy_data, df_data, numpy_vanilla_rnn_data, df_vanilla_rnn_data = x.get_data()
+
+    X = df_data[["Open", "High", "Low", "Close", "Adj Close", "Volume", "Twitter Score"]]
+
+    # Shift to get the previous data as next time step X
+    X[["Open", "High", "Low", "Close", "Adj Close", "Volume"]] = X[["Open", "High", "Low", "Close", "Adj Close", "Volume"]].shift(-1)
+    y =  df_data["Close"]
+
+    # Remove last step that now has a NaN in shifted values
+    X = X[:-1]
+    y = y[:-1]
+
+    # Manually make test_size last 20 percent
+    dataset_size = len(X)
+    train_prob = 1 - test_prob
+    split_point = int(np.round(dataset_size * train_prob))
+
+    train_data = X.iloc[:split_point,:]
+    test_data = X.iloc[split_point:, :]
+    train_prices = y[:split_point]
+    test_prices = y[split_point:]
+
+    # If want to use without the twitter data for BASELINE MODEL
+    #X_train_no_twit = X_train["Open", "High", "Low", "Close", "Adj Close", "Volume"]
+    #X_test_no_twit = X_test["Open", "High", "Low", "Close", "Adj Close", "Volume"]
+
+    # Convert out of dataframes for use in numpy
+    train_data = train_data.to_numpy().astype(np.float32)
+    test_data = test_data.to_numpy().astype(np.float32)
+    train_prices = train_prices.to_numpy().astype(np.float32)
+    test_prices = test_prices.to_numpy().astype(np.float32)
+
+
     return train_data, test_data, train_prices, test_prices
 
 def slidingWindow(array, window_size):
@@ -95,17 +154,34 @@ def slidingWindow(array, window_size):
         windowed_array.append(window)
     return np.array(windowed_array)
 
+def sliding_window_test(arr, train_data, window_size):
+    # Grab window size - 1 from the end of the training set 
+    start = train_data.shape[0] - (window_size - 1) 
+    end_of_training = train_data[start:, :]
+    stacked = np.vstack([end_of_training, arr])
+
+    # Do the same thing as sliding window 
+    arr = slidingWindow(stacked, window_size)
+    return arr
+    
+
 def preprocess(train_data, test_data, train_prices, test_prices, window_size):
-    return slidingWindow(train_data,window_size), slidingWindow(test_data,window_size), train_prices[window_size-1:], test_prices[window_size-1:]
+    # FIXME: This is the previous implementation if you want to run on other data
+    #return slidingWindow(train_data,window_size), slidingWindow(test_data, window_size), train_prices[window_size-1:], test_prices[window_size-1:]
+
+    # New one that uses the twitter data
+    return slidingWindow(train_data,window_size), sliding_window_test(test_data, train_data, window_size), train_prices[window_size-1:], test_prices
     
 if __name__=="__main__":
-    rnn = Stock_RNN(24)
+    BATCH_SIZE = 12
     TEST_PROB = 0.2
-    NUM_EPOCHS = 5000
-    WINDOW_SIZE = 24
+    NUM_EPOCHS = 3000
+    WINDOW_SIZE = 16
+
+    rnn = Stock_RNN(BATCH_SIZE)
     train_data, test_data, train_prices, test_prices = get_data(TEST_PROB)
     train_data, test_data, train_prices, test_prices = preprocess(train_data, test_data, train_prices, test_prices, WINDOW_SIZE)
-    #train_data, test_data, train_prices, test_prices = train_data[...,None], test_data[...,None], train_prices[...,None], test_prices[...,None]
+    train_data, test_data, train_prices, test_prices = train_data[...,None], test_data[...,None], train_prices[...,None], test_prices[...,None]
     print(np.shape(train_data))
     print(np.shape(test_data))
     print(np.shape(train_prices))
@@ -113,8 +189,8 @@ if __name__=="__main__":
     #train_data, train_prices = np.arange(441).reshape((441,1)).astype(np.float32), np.arange(441).reshape((441,1)).astype(np.float32)
     train(rnn, train_data, train_prices, test_data, test_prices, NUM_EPOCHS)
     test(rnn, test_data, test_prices)
-    real = plt.plot(train_prices, label='real')
-    pred = plt.plot(rnn(train_data), label='predicted')
+    real = plt.plot(test_prices, label='real')
+    pred = plt.plot(rnn(test_data), label='predicted')
     
     plt.legend(['Real', 'Predicted'])
     
--- python_scripts/testing.py
+++ python_scripts/testing.py
@@ -12,25 +12,61 @@ from preprocess import Preprocess
 # Playing around with seaborn
 import seaborn as sns
 
-sns.set_style("ticks")
-sns.set_style("darkgrid")
+# sns.set_style("ticks")
+# sns.set_style("darkgrid")
 
-# # Get the data
-# STOCK_DATABASE_PATH = "../data/stock_data.db"
-# RNN_DATABASE_PATH = "../data/rnn_data.db"
+# # # Get the data
+# # STOCK_DATABASE_PATH = "../data/stock_data.db"
+# # RNN_DATABASE_PATH = "../data/rnn_data.db"
 
-# x = Preprocess(STOCK_DATABASE_PATH, RNN_DATABASE_PATH)
-# numpy_data, df_data, numpy_vanilla_rnn_data, df_vanilla_rnn_data = x.get_data()
+# # x = Preprocess(STOCK_DATABASE_PATH, RNN_DATABASE_PATH)
+# # numpy_data, df_data, numpy_vanilla_rnn_data, df_vanilla_rnn_data = x.get_data()
 
-# # Get data in nice format with correct lagging 
+# # # Get data in nice format with correct lagging 
 
-x, y, z = np.random.rand(3, 100)
+# x, y, z = np.random.rand(3, 100)
 
-#cmap = sns.cubehelix_palette(as_cmap=True)
-cmap = sns.diverging_palette(10, 133, sep=3, as_cmap=True, center="dark")
-#cmap = sns.light_palette("green", as_cmap=True)
+# #cmap = sns.cubehelix_palette(as_cmap=True)
+# cmap = sns.diverging_palette(10, 133, sep=3, as_cmap=True, center="dark")
+# #cmap = sns.light_palette("green", as_cmap=True)
 
-f, ax = plt.subplots()
-points = ax.scatter(x, y, c=z, s=50, cmap=cmap, marker="8")
-f.colorbar(points, label="meow")
-plt.show()
+# f, ax = plt.subplots()
+# points = ax.scatter(x, y, c=z, s=50, cmap=cmap, marker="8")
+# f.colorbar(points, label="meow")
+# plt.show()
+
+def get_data(test_prob=0.2):
+
+    # Parsing actual data that we will use
+    STOCK_DATABASE_PATH = "../data/stock_data.db"
+    RNN_DATABASE_PATH = "../data/rnn_data.db"
+    x = Preprocess(STOCK_DATABASE_PATH, RNN_DATABASE_PATH)
+    numpy_data, df_data, numpy_vanilla_rnn_data, df_vanilla_rnn_data = x.get_data()
+
+    X = df_data[["Open", "High", "Low", "Close", "Adj Close", "Volume", "Twitter Score"]]
+
+    # Shift to get the previous data as next time step X
+    X[["Open", "High", "Low", "Close", "Adj Close", "Volume"]] = X[["Open", "High", "Low", "Close", "Adj Close", "Volume"]].shift(-1)
+    y =  df_data["Close"]
+
+    # Remove last step that now has a NaN in shifted values
+    X = X[:-1]
+    y = y[:-1]
+
+    # Manually make test_size last 20 percent
+    dataset_size = len(X)
+    train_prob = 1 - test_prob
+    split_point = int(np.round(dataset_size * train_prob))
+
+    train_data = X.iloc[:split_point,:]
+    test_data = X.iloc[split_point:, :]
+    train_prices = y[:split_point]
+    test_prices = y[split_point:]
+
+    # Without the twitter data X_train and X_test
+    #X_train_no_twit = X_train["Open", "High", "Low", "Close", "Adj Close", "Volume"]
+    #X_test_no_twit = X_test["Open", "High", "Low", "Close", "Adj Close", "Volume"]
+
+    return train_data, test_data, train_prices, test_prices
+
+get_data()
